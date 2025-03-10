94ff17e480b2669d69013c595417b5904f723852
Abdullah31012005 <qsm4235@gmail.com>
2025-03-10 21:23:33 +0500

    My Portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+function openHospital() {
+    window.location.href="https://github.com/Abdullah31012005/All-Projects/tree/main/DSA%20Project";
+}
+function openCoal(){
+    window.location.href="https://github.com/Abdullah31012005/All-Projects/tree/main/Coal%20Project";
+}
+function openAcp(){
+    window.location.href="https://github.com/Abdullah31012005/All-Projects/tree/main/ACP-Project";
+}
+function openCC(){
+    window.location.href="https://github.com/Abdullah31012005/All-Projects/tree/main/CC%20Project";
+}
